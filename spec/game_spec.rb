0905b540239fcf8b17d53a1c1f88a808382a108b
Jon Hallam <jonathan.hallam@digital.cabinet-office.gov.uk>
2017-03-02 16:15:12 +0000

    Now works on most normal level problems

--- spec/game_spec.rb
+++ spec/game_spec.rb
@@ -12,26 +12,16 @@ describe Game do
                                               [6, 0, 6], [6, 2, 8], [6, 6, 9], [6, 7, 1],
                                               [7, 2, 7], [7, 4, 4], [7, 8, 5],
                                               [8, 1, 2], [8, 4, 9], [8, 7, 3], [8, 8, 8]  ])}
-  let!(:game2) {@game = Game.new(Board.new, [ [0, 0, 7], [0, 1, 6], [0, 4, 5], [0, 7, 8],
-                                              [1, 0, 4], [1, 4, 3], [1, 6, 2],
-                                              [2, 1, 3], [2, 2, 2], [2, 6, 4], [2, 8, 6],
-                                              [3, 5, 8],
-                                              [4, 2, 9], [4, 4, 6], [4, 6, 8],
-                                              [5, 3, 2],
-                                              [6, 0, 6], [6, 2, 8], [6, 6, 9], [6, 7, 1],
-                                              [7, 2, 7], [7, 4, 4], [7, 8, 5],
-                                              [8, 1, 2], [8, 4, 9], [8, 7, 3], [8, 8, 8]  ])}
-  let!(:game3) {@game = Game.new(Board.new, [ [0, 0, 7], [0, 1, 6], [0, 4, 5], [0, 7, 8],
-                                              [1, 0, 4], [1, 4, 3], [1, 6, 2],
-                                              [2, 1, 3], [2, 2, 2], [2, 6, 4], [2, 8, 6],
-                                              [3, 5, 8],
-                                              [4, 2, 9], [4, 4, 6], [4, 6, 8],
-                                              [5, 3, 2],
-                                              [6, 0, 6], [6, 2, 8], [6, 6, 9], [6, 7, 1],
-                                              [7, 2, 7], [7, 4, 4], [7, 8, 5],
-                                              [8, 1, 2], [8, 4, 9], [8, 7, 3], [8, 8, 8]  ])}
-
-
+  let!(:hard_problem) {@game = Game.new(Board.new, [  [0, 0, 1], [0, 4, 7], [0, 7, 3], [0, 8, 2],
+                                                      [1, 0, 8], [1, 1, 3], [1, 3, 6],
+                                                      [2, 2, 2], [2, 3, 9], [2, 6, 6], [2, 8, 8],
+                                                      [3, 0, 6], [3, 5, 4], [3, 6, 9], [3, 8, 7],
+                                                      [4, 1, 9], [4, 7, 5],
+                                                      [5, 0, 3], [5, 2, 7], [5, 3, 5], [5, 8, 4],
+                                                      [6, 0, 2], [6, 2, 3], [6, 5, 9], [6, 6, 1],
+                                                      [7, 5, 2], [7, 7, 4], [7, 8, 3],
+                                                      [8, 0, 7], [8, 1, 4], [8, 4, 8], [8, 8, 9]
+    ])}
   it "should initialize properly" do
     expect(life.is_a?(Game)).to eql(true)
     expect(life.game_world.is_a?(Board)).to eql(true)
@@ -55,8 +45,7 @@ describe Game do
                                                 [0, 2, 0, 0, 9, 0, 0, 3, 8] ])
     end
 
-  it "should remove possibilities if the value is set" do
-    game.remove_possibilities
+  it "should remove possibilities if the value is set on initialization" do
     expect(game.game_world.board[0][0].possibilities.length).to eql(1)
     expect(game.game_world.board[0][2].possibilities.length).to eql(9)
   end
@@ -68,15 +57,69 @@ describe Game do
   end
 
   it "should remove possibilities based on what's in the column" do
-    game2.remove_column_possibilities
-    expect(game2.game_world.board[0][2].possibilities).to eql([1, 3, 4, 5, 6])
-    expect(game2.game_world.board[1][1].possibilities).to eql([1, 4, 5, 7, 8, 9])
+    game.remove_column_possibilities
+    expect(game.game_world.board[0][2].possibilities).to eql([1, 3, 4, 5, 6])
+    expect(game.game_world.board[1][1].possibilities).to eql([1, 4, 5, 7, 8, 9])
   end
 
   it "should remove possibilities based on what's in the blocks" do
-    game3.remove_block_possibilities
-    expect(game3.game_world.board[4][6].possibilities).to eql([1, 2, 3, 5, 6, 7, 8])
-    expect(game3.game_world.board[8][3].possibilities).to eql([1, 3, 4, 5, 9])
+    game.remove_block_possibilities
+    expect(game.game_world.board[4][7].possibilities).to eql([1, 2, 3, 4, 5, 6, 7, 9])
+    expect(game.game_world.board[8][2].possibilities).to eql([1, 3, 4, 5, 9])
+  end
+
+  it "should remove possibilities from all three" do
+    game.remove_row_possibilities
+    game.remove_column_possibilities
+    game.remove_block_possibilities
+    expect(game.game_world.board[3][6].possibilities).to eql([1, 3, 5, 6, 7])
+    expect(game.game_world.board[5][7].possibilities).to eql([4, 5, 6, 7, 9])
+  end
+
+  it "should check to see if any cell have new unique possibilities and remove set possibilities from other " do
+    game.remove_row_possibilities
+    game.remove_column_possibilities
+    game.remove_block_possibilities
+    game.check_unique_cell_values
+    expect(game.game_world.board[0][2].possibilities).to eql([1])
+    expect(game.game_world.board[0][2].set).to eql(true)
+    expect(game.game_world.board[0][2].num).to eql(1)
+  end
+
+  it "should search through the possibilities for all cells in a row for unique values" do
+    game.remove_row_possibilities
+    game.remove_column_possibilities
+    game.remove_block_possibilities
+    game.check_unique_and_update_row_possibilities
+    expect(game.game_world.board[0][5].possibilities).to eql([2])
+    expect(game.game_world.board[0][5].set).to eql(true)
+    expect(game.game_world.board[0][5].num).to eql(2)
+  end
+
+  it "should search through the possibilities for all cells in a column for unique values" do
+    game.remove_row_possibilities
+    game.remove_column_possibilities
+    game.remove_block_possibilities
+    game.check_unique_and_update_column_possibilities
+    expect(game.game_world.board[0][0].num).to eql(7)
+  end
+
+  it "should search through the possibilities for all cells in a block for unique values" do
+    game.remove_row_possibilities
+    game.remove_column_possibilities
+    game.remove_block_possibilities
+    game.check_unique_and_update_block_possibilities
+    expect(game.game_world.board[6][8].num).to eql(4)
+  end
+
+  it "should loop all this until it's finished" do
+    game.solve_that
+    expect(game.game_world.show_board.flatten.include?(0)).to eql(false)
+  end
+
+  it "should solve hard problems too!" do
+    hard_problem.solve_that
+    expect(hard_problem.game_world.show_board.flatten.include?(0)).to eql(false)
   end
 
 
